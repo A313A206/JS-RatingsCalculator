31cfc68cd4621d10a9e98158971f704e729d5e84
A313A206 <55757642+A313A206@users.noreply.github.com>
2022-03-18 15:50:57 -0400

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -1,4 +1,3 @@
-const { each } = require("cheerio/lib/api/traversing");
 
 const collect_ratings = () => {
     const ratings = {"count": 0, "sum": 0, "average": 0};
@@ -11,4 +10,15 @@ const collect_ratings = () => {
         ratings.count += parseInt(element.value);
         ratings.sum += parseInt(element.value) * rating;
     });
+
+    if (ratings.count !== 0) {
+        ratings.average = ratings.sum / ratings.count;
+    }
+    
+    return ratings;
 };
+    
+document.addEventListener('change', () => {
+    const ratings = collect_ratings();
+    document.querySelector('#average').value = ratings.average.toFixed(2);
+});
